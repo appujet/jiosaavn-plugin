14e003f4c705c352aa5c0d3e715860b4358928ac
appujet <sdipedit@gmail.com>
2024-04-29 02:26:41 +0530

    Update release-beta.yml workflow to fix Gradle wrapper permissions and existence check

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     `java-library`
     `maven-publish`
     alias(libs.plugins.lavalink)
+    id("com.github.johnrengelman.shadow")
+    id("com.github.breadmoirai.github-release")
 }
 
 val pluginVersion = Version(0, 1, 0)
@@ -25,15 +27,39 @@ dependencies {
     implementation(projects.main)
 }
 
+val impl = project.configurations.implementation.get()
+impl.isCanBeResolved = true
+
 tasks {
     jar {
         archiveBaseName.set(archivesBaseName)
     }
+    shadowJar {
+        archiveBaseName.set(archivesBaseName)
+        archiveClassifier.set("")
+
+        configurations = listOf(impl)
+    }
+    build {
+        dependsOn(processResources)
+        dependsOn(compileJava)
+        dependsOn(shadowJar)
+    }
+    publish {
+        dependsOn(publishToMavenLocal)
+    }
+}
+
+tasks.githubRelease {
+    dependsOn(tasks.jar)
+    dependsOn(tasks.shadowJar)
+    mustRunAfter(tasks.shadowJar)
 }
 
 data class Version(val major: Int, val minor: Int, val patch: Int) {
     override fun toString() = "$major.$minor.$patch"
 }
+
 
 publishing {
     publications {
@@ -43,5 +69,31 @@ publishing {
     }
     repositories {
         maven(url = "https://jitpack.io")
+    }
+}
+
+githubRelease {
+    token(System.getenv("GITHUB_TOKEN"))
+    owner("appujet")
+    repo("jiosaavan-plugin")
+    targetCommitish(System.getenv("RELEASE_TARGET"))
+    releaseAssets(tasks.shadowJar.get().outputs.files.toList())
+    tagName(verName)
+    releaseName(verName)
+    overwrite(false)
+    prerelease(preRelease)
+
+    if (preRelease) {
+        body("""Here is a pre-release version of the plugin. Please test it and report any issues you find.
+            |Example:
+            |```yml
+            |lavalink:
+            |    plugins:
+            |        - dependency: "com.github.appujet:jiosaavan-plugin:$verName"
+            |          repository: https://jitpack.io
+            |```
+        """.trimMargin())
+    } else {
+        body(changelog())
     }
 }
