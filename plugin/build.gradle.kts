6a026008f01697fc7ef9a32d3c8b145a5a7f5af3
appujet <sdipedit@gmail.com>
2024-04-29 02:10:52 +0530

    Update release-beta.yml workflow to fix Gradle wrapper permissions and existence check

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -1,11 +1,7 @@
-
 plugins {
     `java-library`
     `maven-publish`
-
     alias(libs.plugins.lavalink)
-    id("com.github.johnrengelman.shadow")
-    id("com.github.breadmoirai.github-release")
 }
 
 val pluginVersion = Version(0, 1, 0)
@@ -16,11 +12,11 @@ val archivesBaseName = "jiosaavn-plugin"
 val preRelease = System.getenv("PRERELEASE") == "true"
 val verName = "${if (preRelease) "PRE_" else ""}$pluginVersion${if(preRelease) "_${System.getenv("GITHUB_RUN_NUMBER")}" else ""}"
 
-
 lavalinkPlugin {
     name = "jiosaavn-plugin"
     path = "$group.plugin"
     version = verName
+    // Assuming libs and versions are defined elsewhere
     apiVersion = libs.versions.lavalink.api
     serverVersion = libs.versions.lavalink.server
 }
@@ -29,33 +25,10 @@ dependencies {
     implementation(projects.main)
 }
 
-val impl = project.configurations.implementation.get()
-impl.isCanBeResolved = true
-
 tasks {
     jar {
         archiveBaseName.set(archivesBaseName)
     }
-    shadowJar {
-        archiveBaseName.set(archivesBaseName)
-        archiveClassifier.set("")
-
-        configurations = listOf(impl)
-    }
-    build {
-        dependsOn(processResources)
-        dependsOn(compileJava)
-        dependsOn(shadowJar)
-    }
-    publish {
-        dependsOn(publishToMavenLocal)
-    }
-}
-
-tasks.githubRelease {
-    dependsOn(tasks.jar)
-    dependsOn(tasks.shadowJar)
-    mustRunAfter(tasks.shadowJar)
 }
 
 data class Version(val major: Int, val minor: Int, val patch: Int) {
@@ -63,46 +36,12 @@ data class Version(val major: Int, val minor: Int, val patch: Int) {
 }
 
 publishing {
-    repositories {
-        maven {
-            url = if (preRelease) {
-                uri("https://maven.lavalink.dev/snapshots")
-            } else {
-                uri("https://maven.lavalink.dev/releases")
-            }
-            credentials {
-                username = System.getenv("USERNAME")
-                password = System.getenv("PASSWORD")
-            }
-            authentication {
-                create<BasicAuthentication>("basic")
-            }
+    publications {
+        create<MavenPublication>("jitpack") {
+            from(components["java"])
         }
     }
-}
-
-githubRelease {
-    token(System.getenv("GITHUB_TOKEN"))
-    owner("appujet")
-    repo("jiosaavn-plugin")
-    targetCommitish(System.getenv("RELEASE_TARGET"))
-    releaseAssets(tasks.shadowJar.get().outputs.files.toList())
-    tagName(verName)
-    releaseName(verName)
-    overwrite(false)
-    prerelease(preRelease)
-
-    if (preRelease) {
-        body("""This is a pre-release version. Please do not use it in production. To use it set `snapshot` to true in your plugin configuration.
-            |Example:
-            |```yml
-            |lavalink:
-            |    plugins:
-            |        - dependency: "com.github.appujet:jiosaavn-plugin:$verName"
-            |          snapshot: true
-            |```
-        """.trimMargin())
-    } else {
-        body(changelog())
+    repositories {
+        maven(url = "https://jitpack.io")
     }
 }
