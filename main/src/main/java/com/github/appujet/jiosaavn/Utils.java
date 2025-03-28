ffe2e4236ba8a014bc44963e7c3e3709a4d96328
appujet <sdipedit@gmail.com>
2025-03-28 11:29:30 +0530

    refactor decryption method and update encryption algorithm

--- main/src/main/java/com/github/appujet/jiosaavn/Utils.java
+++ main/src/main/java/com/github/appujet/jiosaavn/Utils.java
@@ -1,16 +1,17 @@
 package com.github.appujet.jiosaavn;
 
 
+import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
+
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
-import java.security.Key;
 import java.util.Base64;
 
 public class Utils {
 
     private static final String KEY = "38346591";
-    private static final String ALGORITHM = "DES/ECB/NoPadding";
-    private static final String[] QUALITIES = { "_320", "_160", "_96", "_48", "_12" };
+    private static final String ALGORITHM = "DES";
+    private static final String TRANSFORMATION = "DES/ECB/PKCS5Padding";
     
     /**
      * Generates a download link by decrypting the encrypted media URL.
@@ -18,33 +19,16 @@ public class Utils {
      * @param encryptedMediaUrl The encrypted media URL.
      * @return The download link or null if not valid.
      */
-    public static String getDownloadLink(String encryptedMediaUrl) {
-        if (encryptedMediaUrl == null || encryptedMediaUrl.isEmpty())
-            return null;
-
+    public static String decryptUrl(String url) {
         try {
-            byte[] encryptedBytes = Base64.getDecoder().decode(encryptedMediaUrl);
-            Key key = new SecretKeySpec(KEY.getBytes(), "DES");
-            Cipher cipher = Cipher.getInstance(ALGORITHM);
-            cipher.init(Cipher.DECRYPT_MODE, key);
-
+            byte[] encryptedBytes = Base64.getDecoder().decode(url);
+            SecretKeySpec keySpec = new SecretKeySpec(KEY.getBytes(), ALGORITHM);
+            Cipher cipher = Cipher.getInstance(TRANSFORMATION);
+            cipher.init(Cipher.DECRYPT_MODE, keySpec);
             byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
-            String decryptedLink = new String(decryptedBytes).trim();
-
-            for (String quality : QUALITIES) {
-                String downloadUrl = decryptedLink.replace("_96", quality);
-                if (isValidUrl(downloadUrl)) {
-                    return downloadUrl;
-                }
-            }
+            return new String(decryptedBytes);
         } catch (Exception e) {
-            e.printStackTrace();
+            throw new FriendlyException("Failed to decrypt URL", FriendlyException.Severity.COMMON, e);
         }
-        return null;
-    }
-
-    // Helper method to check if a URL is valid
-    private static boolean isValidUrl(String url) {
-        return url.startsWith("https://aac.saavncdn.com/") && url.endsWith(".mp4");
     }
 }
